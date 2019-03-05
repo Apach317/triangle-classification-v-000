694a53ea1d906a7993e3b97353790801957912f1
Ron Lynch <lynch.ronald1@gmail.com>
2019-03-05 15:23:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :sides, :equilateral, :isosceles, :scalene
+  
+  def initialization(S1, S2, S3)
+    @sides = []
+    @sides << S1
+    @sides << S2
+    @sides << S3
+  end
+
+  def kind
+    if (sides[0] >= sides[1] + sides[2]) || (sides[1] >= sides[0] + sides[2]) || (sides[2] >= sides[0] + sides[1])
+      raise TriangleError
+    else
+      if sides.uniq.count == 1
+        :equilateral
+      elsif sides.uniq.count == 2
+        :isosceles
+      else sides.uniq.count == 3
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+
+    def message
+      "No, No, No! Thats not a Triangle"
+    end
+  end
+
+    
 end
